3bbf537f99c461e1269d7f0db29f35cd95213b22
rudol <rudol@DESKTOP-3QDJPOU>
2024-03-07 21:55:55 +0100

    Projektdateien hinzufügen.

---
+++ BlazorApp1/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorApp1
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int amount { get; set; } = 1;
+        public string? productname { get; set; }
+        public double price { get; set; }
+
+        public double totalprice { get; set; }
+    }
+}
